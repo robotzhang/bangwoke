264c51ed1c5d90c377ac3a1d82440eac56939b3b
robotzhang <445679586@qq.com>
2012-08-23 20:48:28 +0800

    logo图片地址更改

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,6 +1,6 @@
 <header class="header">
     <div class="container">
-        <a href="/"><img src="/bangwoke/static/images/logo.png"/>快速获取珍藏</a>
+        <a href="/"><img src="<?php echo base_url() ?>/static/images/logo.png"/>快速获取珍藏</a>
         <ul class="member pull-right unstyled">
             <li class="first"><a href="/account/sign_up">注册</a></li>
             <li class="last"><a href="/account/sign_in">登录</a></li>
