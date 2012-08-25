407a50b3183a16aeb1e074416bdd1562c120a9da
robotzhang <445679586@qq.com>
2012-08-25 14:28:17 +0800

    去除url中多余的  /

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,6 +1,6 @@
 <header class="header">
     <div class="container">
-        <a href="/"><img src="<?php echo base_url() ?>/static/images/logo.png"/>快速获取值得珍藏的电影</a>
+        <a href="/"><img src="<?php echo base_url() ?>static/images/logo.png"/>快速获取值得珍藏的电影</a>
         <ul class="member pull-right unstyled">
             <li class="first"><a href="javascript:void(0);">注册</a></li>
             <li class="last"><a href="javascript:void(0);">登录</a></li>
