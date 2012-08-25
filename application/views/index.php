14a32456d89d16d81b252e6bbb56a86a9de58338
robotzhang <445679586@qq.com>
2012-08-25 22:28:37 +0800

    base_url()有/后缀  site__url()居然没有

--- application/views/index.php
+++ application/views/index.php
@@ -2,7 +2,7 @@
     <?php foreach ($topics as $key => $topic): ?>
     <div class="span6">
         <h3 style="font-weight: normal;">
-            <a style="color: #333;" href="<?php echo site_url().'topics/'.$topic->id ?>"><?php echo $topic->name ?></a>
+            <a style="color: #333;" href="<?php echo site_url().'/topics/'.$topic->id ?>"><?php echo $topic->name ?></a>
         </h3>
         <blockquote><?php echo $topic->summry ?></blockquote>
         <ol>
