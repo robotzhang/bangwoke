88ae2f364abeee58f74b3b22832c1685960f4c04
robotzhang <445679586@qq.com>
2012-09-02 11:21:25 +0800

    去掉url中index.php 线上必须要加个?

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -3,6 +3,8 @@
         <div>
             &copy;2012 BangWoKe All rights reserved.
             <div class="pull-right">
+                <a href="<?php echo site_url() ?>/movies">电影大全</a> -
+                <a href="<?php echo site_url() ?>/topics">影集大全</a> -
                 <a href="<?php echo site_url() ?>/about">关于本站</a> -
                 <a href="">意见反馈</a>
             </div>
