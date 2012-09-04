96cb7ada88b40a10a977bd6b9ae3e92f508356ce
robotzhang <445679586@qq.com>
2012-09-04 15:37:20 +0800

    首页下啦菜单

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -2,23 +2,38 @@
     <div class="container">
         <a href="/"><img src="<?php echo base_url('static/images/logo.png') ?>"/>快速获取值得珍藏的电影</a>
         <div class="member clearfix pull-right">
-            <?php if (is_login()): ?>
             <a class="btn" style="margin: 15px;" href="<?php echo site_url('cart') ?>"><i class="icon-shopping-cart"></i>
                 购物车 <span style="color: #DA4F49;"><?php echo $this->cart->total_items() ?></span>
             </a>
-            欢迎：<?php echo substr(current_user()->email, 0, strpos(current_user()->email, '@')); ?>
-            <a href="<?php echo site_url('logout?url='.current_url()) ?>"><i class="icon-off"></i>退出</a>
+            <?php if (is_login()): ?>
+                <a href="javascript:void(0);" class="show_drop_menu">
+                    欢迎：<?php echo substr(current_user()->email, 0, strpos(current_user()->email, '@')); ?>
+                    <i class="icon-chevron-down"></i>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="javascript:void(0);"><i class="icon-inbox"></i> 我的订单</a></li>
+                    <li class="divider"></li>
+                    <li><a href="<?php echo site_url('logout?url='.current_url()) ?>"><i class="icon-off"></i> 退出</a></li>
+                </ul>
             <?php else: ?>
-            <ul class="unstyled clearfix pull-right">
-                <li>
-                    <a class="btn" style="margin: 15px;" href="<?php echo site_url('cart') ?>"><i class="icon-shopping-cart"></i>
-                        购物车 <span style="color: #DA4F49;"><?php echo $this->cart->total_items() ?></span>
-                    </a>
-                </li>
-                <li class="first"><a href="<?php echo site_url('register?url='.current_url()) ?>">注册</a></li>
-                <li class="last"><a href="<?php echo site_url('login?url='.current_url()) ?>">登录</a></li>
-            </ul>
+                <a href="<?php echo site_url('register?url='.current_url()) ?>">注册</a> -
+                <a href="<?php echo site_url('login?url='.current_url()) ?>">登录</a>
             <?php endif ?>
         </div>
     </div>
 </div>
+<script>
+    $(document).ready(function(){
+        $('.show_drop_menu').click(function() {
+            $('.dropdown-menu').css({
+                top:$(this).offset().top + $(this).outerHeight(),
+                left:$(this).offset().left - ($('.dropdown-menu').outerWidth() - $(this).outerWidth()),
+                right: 'auto'
+            }).toggle();
+            return false;
+        });
+        $(document).click(function(){
+            $('.dropdown-menu').hide();
+        });
+    });
+</script>
