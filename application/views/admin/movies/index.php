6dc1c0d37719cb17e8cb897256c161ef372c86ca
robotzhang <445679586@qq.com>
2012-09-06 02:46:13 +0800

    优化后台电影管理

--- application/views/admin/movies/index.php
+++ application/views/admin/movies/index.php
@@ -11,24 +11,31 @@
 <table class="table table-bordered table-striped">
     <thead>
         <tr>
-            <th width="40">id</th>
+            <th>id</th>
             <th>名称</th>
-            <th width="40">影集</th>
-            <th width="40">豆瓣id</th>
-            <td width="50">操作</td>
+            <th>影集</th>
+            <th >豆瓣id</th>
+            <th>操作</th>
         </tr>
     </thead>
     <tbody>
         <?php foreach ($movies as $movie): ?>
         <tr>
-            <td><?php echo $movie->id ?></td>
-            <td><?php echo movie_name($movie) ?></td>
-            <td><?php echo $movie->topic_id ?></td>
-            <td><?php echo $movie->douban_id ?></td>
+            <td width="40"><?php echo $movie->id ?></td>
             <td>
+                <?php if (!empty($movie->player)): ?>
+                <i class="icon-play-circle" title="有播放地址"></i>
+                <?php endif ?>
+                <?php echo movie_name($movie) ?>
+            </td>
+            <td  width="40"><?php echo $movie->topic_id ?></td>
+            <td width="40"><?php echo $movie->douban_id ?></td>
+            <td width="70">
                 <a title="编辑" href="<?php echo site_url('admin/movies/edit?id='.$movie->id) ?>"><i class="icon-edit"></i></a>
-                |
+                -
                 <a title="删除" onclick="if(!confirm('确定删除？'))return false;" href="<?php echo site_url('admin/movies/delete?id='.$movie->id) ?>"><i class="icon-trash"></i></a>
+                -
+                <a target="_blank" title="预览" href="<?php echo site_url('movies/'.$movie->id) ?>"><i class="icon-file"></i></a>
             </td>
         </tr>
         <?php endforeach ?>
